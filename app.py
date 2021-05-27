0cb6cad73da8d2abcf4003a5dbdf1d57b86988f8
SteveRNewman <stevenewmanphotography@gmail.com>
2021-05-26 22:28:22 -0400

    tables

--- app.py
+++ app.py
@@ -9,15 +9,18 @@ st.set_page_config(page_title="Virufy Dashboard", layout='centered')
 
 results_df = pd.read_csv("results.csv")
 
-st.title("How well does Model A predict Covid?")
+st.title("How well do the models predict Covid?")
 
 country_list = list(results_df.country.unique())
+model_list = ["Model_1", "Model_2", "Model_3", "Model_4", "Model_5"]
+
 country = st.sidebar.selectbox("Country",options=country_list)
+model = st.sidebar.selectbox("Model",options=model_list)
 
 results_df_country = results_df[results_df['country']==country]
 
 pcr_test_result = results_df_country['pcr_test_result']
-pcr_test_result_pred = results_df_country['pcr_test_result_pred']
+pcr_test_result_pred = results_df_country[model]
 
 confusion = confusion_matrix(pcr_test_result, pcr_test_result_pred)
 FN = confusion[1][0]
@@ -25,7 +28,7 @@ TN = confusion[0][0]
 TP = confusion[1][1]
 FP = confusion[0][1]
 
-chart = st.sidebar.radio('Chart Type',('DataFrame','Bar Chart','Pie Chart'))
+chart = st.sidebar.radio('Chart Type',('Accuracy','Bar Chart','Pie Chart'))
 
 if chart == 'Pie Chart':
     fig, ax = plt.subplots()
@@ -37,13 +40,39 @@ if chart == 'Bar Chart':
     ax.bar(['False Negative' , 'True Negative' , 'True Positive' , 'False Positive'],[FN,TN,TP,FP])
     st.pyplot(fig)
 
-sensitivity = st.subheader("Sensitivity: {}%".format(round(TP/(TP+FN))))
-specificity = st.subheader("Specificity: {}%".format (round(TN/(TN+FP))))
-ppv = st.subheader("PPV: {}%".format (round(TP/(TP+FP))))
-npv = st.subheader("NPV: {}%".format (round(TN/(FN+TN))))
+sens = (round(TP/(TP+FN)))
+spec = (round(TN/(TN+FP)))
+ppv = (round(TP/(TP+FP)))
+npv = (round(TN/(FN+TN)))
 
-if chart == 'DataFrame':
-    df = pd.DataFrame(index= ["Sensitivity", "Specificity", "PPV", "NPV"],
+df_index = ["Sensitivity", "Specificity", "PPV", "NPV"]
+if chart == 'Accuracy':
+    st.subheader("Results for: {}".format(country_list[0]))
+    df = pd.DataFrame(index=df_index,
        data =  np.random.randn(4, 5),
-        columns=('MODEL %d' % i for i in range(1,6)))
+        columns=(model_list))
+    st.dataframe(df.style.highlight_max(axis=0))
+
+    st.subheader("Results for: {}".format(country_list[1]))
+    df = pd.DataFrame(index=df_index,
+       data =  np.random.randn(4, 5),
+        columns=(model_list))
+    st.dataframe(df.style.highlight_max(axis=0))
+
+    st.subheader("Results for: {}".format(country_list[2]))
+    df = pd.DataFrame(index= df_index,
+       data =  np.random.randn(4, 5),
+        columns=(model_list))
+    st.dataframe(df.style.highlight_max(axis=0))
+
+    st.subheader("Results for: {}".format(country_list[3]))
+    df = pd.DataFrame(index= df_index,
+       data =  np.random.randn(4, 5),
+        columns=(model_list))
+    st.dataframe(df.style.highlight_max(axis=0))
+
+    st.subheader("Results for: {}".format(country_list[3]))
+    df = pd.DataFrame(index= df_index,
+       data =  np.random.randn(4, 5),
+        columns=(model_list))
     st.dataframe(df.style.highlight_max(axis=0))
